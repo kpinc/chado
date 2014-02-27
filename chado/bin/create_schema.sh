685a6d35b2ddb85579b34829a75c4121aa361c58
Karl O. Pinc <kop@meme.com>
2014-02-27 12:09:00 -0500

    Install: Destroy and re-create all PG schemas used by Chado.

--- chado/bin/create_schema.sh
+++ chado/bin/create_schema.sh
@@ -5,7 +5,15 @@ DBUSER=$3
 DBNAME=$4
 SCHEMA=$5
 
-! cat modules/complete.sql \
+{ printf 'DROP SCHEMA %s CASCADE;\n' $SCHEMA; \
+  printf 'DROP SCHEMA frange CASCADE;\n'; \
+  printf 'DROP SCHEMA genetic_code CASCADE;\n'; \
+  printf 'DROP SCHEMA so CASCADE;\n' ; } \
+  | psql -h $DBHOST -p $DBPORT -U $DBUSER $DBNAME
+printf 'Errors regards missing schemas may be safely ignored.'
+
+! { printf 'CREATE SCHEMA %s;\n' $SCHEMA ; \
+    cat modules/complete.sql ; } \
   | psql -h $DBHOST -p $DBPORT -U $DBUSER $DBNAME 2>&1 \
   | grep -E 'ERROR|FATAL|No such file or directory' \
   && echo "schema $SCHEMA installed in database $DBNAME"
--- chado/bin/test_load.sh
+++ chado/bin/test_load.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 $SHELL create_db.sh $1 $2 $3 $4 \
- && $SHELL create_schema.sh $1 $2 $3 $4
+ && $SHELL create_schema.sh $1 $2 $3 $4 $5
